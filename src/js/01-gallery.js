b947c2771a6097a199a77e5af3a2cfc18e1d79a4
Palamarchuck <palico55@gmail.com>
2022-05-28 16:54:47 +0300

    1 task

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+// galleryList.style.listStyle = "none";
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+        }).join('');
+      
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt", captionPosition: 'bottom', captionDelay: 250,
+});
+
+console.log(lightbox);
