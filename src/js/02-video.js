a1ae619f29054dccd9fe33337e43e15962505c37
Palamarchuck <palico55@gmail.com>
2022-05-30 21:38:33 +0300

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+
+
+function timeUpdate(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+localStorage.setItem('videoplayer-current-time', timeUpdate);
+
+
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time'))).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener( 'submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+
+
+function onTextareaInput() {
+    
+    const inputValue = {
+        email: refs.email.value,
+        message: refs.message.value,
+    };
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(inputValue))
+};
+
+
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+    e.target.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+populateTextarea();
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        const formData = JSON.parse(savedMessage);
+        email.value = formData.email;
+        message.value = formData.message;
+
+    }
+}
